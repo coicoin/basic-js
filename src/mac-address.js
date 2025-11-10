fc27bf768d3772209743848a0385f903af63e230
admin <>
2025-11-11 03:01:40 +0500

    feat: add mac-address test implementation

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,26 @@ const { NotImplementedError } = require('../lib');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function isMAC48Address(n) {
+  let mac48HexDigits = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+    'A', 'B', 'C', 'D', 'E', 'F'
+  ];
+  let macSplited = n.split("-");
+  if (macSplited.length !== 6) {
+    return false;
+  }
+  for (let i = 0; i < macSplited.length; i++) {
+    if (macSplited[i].length !== 2) {
+      return false;
+    }
+
+    if (!mac48HexDigits.includes(macSplited[i][0]) || !mac48HexDigits.includes(macSplited[i][1])) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = {
